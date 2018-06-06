2638eef1fc3a7a6aa895658f16e12d7754d1a33b
omer aksu <omer.aksu1@gmail.com>
2018-06-06 22:26:13 +0300

    - TLIB Solution 03  COMPLETED

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -55,14 +55,33 @@ class Emojifier {
 
         // If there are no faces detected, show a Toast message
         if(faces.size() == 0){
-            Toast.makeText(context, R.string.no_faces_message, Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "No Faces Detected", Toast.LENGTH_SHORT).show();
+        } else {
+            // COMPLETED (2): Iterate through the faces, calling getClassifications() for each face.
+            for (int i = 0; i < faces.size(); ++i) {
+                Face face = faces.valueAt(i);
+
+                // Log the classification probabilities for each face.
+                getClassifications(face);
+            }
         }
 
-        // TODO (2): Iterate through the faces, calling getClassifications() for each face.
+
 
         // Release the detector
         detector.release();
     }
 
-    // TODO (1): Create a static method called getClassifications() which logs the probability of each eye being open and that the person is smiling.
+    // COMPLETD (1): Create a static method called getClassifications() which logs the probability of each eye being open and that the person is smiling.
+    static void getClassifications(Face face) {
+
+        Log.d(LOG_TAG, "getClassifications: smilingProb = " + face.getIsSmilingProbability());
+        Log.d(LOG_TAG, "getClassifications: leftEyeOpenProb = "
+                + face.getIsLeftEyeOpenProbability());
+        Log.d(LOG_TAG, "getClassifications: rightEyeOpenProb = "
+                + face.getIsRightEyeOpenProbability());
+
+    }
+
+
 }
